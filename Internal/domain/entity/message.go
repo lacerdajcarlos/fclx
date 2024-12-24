20bfcd37d53f4d3c290f0a87ea81f5fdebc3fa28
Carlos Lacerda <lacerda.jcarlos@gmail.com>
2024-12-24 14:04:26 -0300

    first commit

---
+++ Internal/domain/entity/message.go
@@ -0,0 +1,53 @@
+package entity
+
+import (
+	"errors"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Message struct {
+	ID        string
+	Role      string
+	Content   string
+	Tokens    int
+	Model     *Model
+	CreatedAt time.Time
+}
+
+func NewMessage(role, content string, model *Model) (*Message, error) {
+
+	totalTokens := CountTokens(content)
+	msg := &Message{
+		ID:        uuid.New().String(),
+		Role:      role,
+		Content:   content,
+		Tokens:    totalTokens,
+		Model:     model,
+		CreatedAt: time.Now(),
+	}
+	if err := msg.Validate(); err != nil {
+		return nil, err
+	}
+	return msg, nil
+}
+func (m *Message) Validate() error {
+	if m.Role != "user" && m.Role != "system" && m.Role != "assistant" {
+		return errors.New("invalid role")
+	}
+	if m.Content == "" {
+		return errors.New("content is empty")
+	}
+	if m.CreatedAt.IsZero() {
+		return errors.New("Invalid created_at")
+	}
+	return nil
+}
+func (m *Message) GetQtdTokens() int {
+	return m.Tokens
+}
+func CountTokens(content string) int {
+	return len(strings.Fields(content))
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/lacerda.jcarlos/fclx/chatservice
+
+go 1.21.0
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/j178/tiktoken-go v0.0.0-20240609130041-b569f671519c
+)
+
+require (
+	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/sashabaranov/go-openai v1.36.0 // indirect
+)
